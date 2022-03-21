e6c04574495cb7456c0b1526c32d828b4a836706
matsamb <matondo.lusamba@gmail.com>
2022-03-21 15:36:10 +0100

    maven site goal operational, bug fixes realized

--- src/main/java/com/safetynet/alert/service/SafetynetalertPersonsService.java
+++ src/main/java/com/safetynet/alert/service/SafetynetalertPersonsService.java
@@ -12,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.safetynet.alert.entity.Persons;
 import com.safetynet.alert.factory.SafetynetalertFactory;
@@ -35,38 +37,55 @@ public class SafetynetalertPersonsService {
 	public void jsonToDatabase() {
 
 		Resource resource = serviceFactory.loadSafetynetAlertDataWithClassPathResource();
-
+		List<String> jsonAsListOfStrings = null;
+		
 		try (BufferedReader finalReader = new BufferedReader(new InputStreamReader(resource.getInputStream()))) {
-			List<String> jsonAsListOfStrings = finalReader.lines().collect(Collectors.toList());// .forEach(System.out::println);
-			serviceLogger.info("json file reader operates correctly");
-
-			String jsonString = String.join("", jsonAsListOfStrings);
-			serviceLogger.trace("json file content turned into a java string");
-
-			String personsJsonArrayString = jsonString.substring(jsonString.indexOf('['), jsonString.indexOf(']') + 1);
-			System.out.println(personsJsonArrayString);
-
-			String firestationsJsonArrayString = jsonString.substring(jsonString.indexOf('[', jsonString.indexOf(']')),
-					jsonString.indexOf(']', jsonString.indexOf(']') + 1) + 1);
-			System.out.println(jsonString.indexOf('[', jsonString.indexOf(']')) + "  "
-					+ jsonString.indexOf(']', jsonString.indexOf(']') + 1) + 1 + "  " + firestationsJsonArrayString);
-
-			String medicalrecordsJsonArrayString = jsonString.substring(
-					jsonString.indexOf('[', jsonString.indexOf(']', jsonString.indexOf(']') + 1) + 1),
-					jsonString.indexOf('}', jsonString.length() - 2));
-			System.out.println(medicalrecordsJsonArrayString);
-
-			ObjectMapper objectMapper = new ObjectMapper();
-
-			Persons[] personsJavaArray = objectMapper.readValue(personsJsonArrayString, Persons[].class);
-			List<Persons> personsJavaList = Arrays.asList(personsJavaArray);
-			safetynetalertPersonsRepository.saveAll(personsJavaList);
-
-			serviceLogger.trace("file reader service get file content correctly");
+			jsonAsListOfStrings = finalReader.lines().collect(Collectors.toList());// .forEach(System.out::println);
+			serviceLogger.info("file reader service get file content correctly");
 		} catch (IOException e) {
 			e.printStackTrace();
 			serviceLogger.error("file reader service can not fetch file content");
 		}
+			
+		String jsonString = String.join("", jsonAsListOfStrings);
+		serviceLogger.trace("json file content turned into a java string");
+
+		String personsJsonArrayString = jsonString.substring(jsonString.indexOf('['), jsonString.indexOf(']') + 1);
+		System.out.println(personsJsonArrayString);
+		serviceLogger.trace("json persons array as string correctly extracted from json source file string");
+
+		String firestationsJsonArrayString = jsonString.substring(jsonString.indexOf('[', jsonString.indexOf(']')),
+					jsonString.indexOf(']', jsonString.indexOf(']') + 1) + 1);
+		System.out.println(firestationsJsonArrayString);
+		serviceLogger.trace("json firestations array as string correctly extracted from json source file string");
+		
+		String medicalrecordsJsonArrayString = jsonString.substring(
+					jsonString.indexOf('[', jsonString.indexOf(']', jsonString.indexOf(']') + 1) + 1),
+					jsonString.indexOf('}', jsonString.length() - 2));
+		System.out.println(medicalrecordsJsonArrayString);
+		serviceLogger.trace("json medicalrecords array as string correctly extracted from json source file string");
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		Persons[] personsJavaArray = null;
+		try {
+			personsJavaArray = objectMapper.readValue(personsJsonArrayString, Persons[].class);
+			serviceLogger.info("json persons array as string correctly mapped to java array");
+
+		} catch(JsonMappingException m) {
+			m.printStackTrace();
+			serviceLogger.error("JsonMappingException, json persons array as string can't be mapped to java array");
+
+		} catch (JsonProcessingException p) {
+			p.printStackTrace();
+			serviceLogger.error("JsonProcessingException, json persons array as string can't be mapped to java array");
+
+		} 
+		List<Persons> personsJavaList = Arrays.asList(personsJavaArray);
+		safetynetalertPersonsRepository.saveAll(personsJavaList);
+		serviceLogger.info("json file persons content loaded into database");
+
+		
 	}
 
 	public void saveAllPersons(Iterable<Persons> persons) {
