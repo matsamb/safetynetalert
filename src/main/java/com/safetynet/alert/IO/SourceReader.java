a7e88e1230c7f0b13df218ee82420de12f51951b
matsamb <matondo.lusamba@gmail.com>
2022-05-07 12:36:18 +0200

    project operational

--- src/main/java/com/safetynet/alert/IO/SourceReader.java
+++ src/main/java/com/safetynet/alert/IO/SourceReader.java
@@ -1,6 +1,5 @@
 package com.safetynet.alert.IO;
 
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 @Service
--- src/main/java/com/safetynet/alert/service/persons/PersonsDaoLoaderService.java
+++ src/main/java/com/safetynet/alert/service/persons/PersonsDaoLoaderService.java
@@ -6,7 +6,6 @@ import java.util.Objects;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
